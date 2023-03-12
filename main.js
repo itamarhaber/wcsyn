ecd42434eb47cc4a743adcdea760689cae5a7688
Itamar Haber <itamar@redis.com>
2023-03-12 23:26:31 +0200

    Choice working

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function fromPx(s) { return Number(s.slice(0,-2)) }
-
+function caller() { return (new Error()).stack.split("\n")[3].trim().split(" ")[1] }
 class Path {
   #x;
 
@@ -30,35 +30,115 @@ class Path {
   }
 
   #canvas;
-  #AR;
-
-  constructor(arcRadius, lineWidth) {
-    this.#AR = arcRadius;
+  #arcRadius;
+  #start;
+  #cx;
+  #cy;
+  #ltr;
+  #ttb;
+  constructor(canvas, arcRadius) {
+    this.#canvas = canvas;
+    this.#arcRadius = arcRadius;
+    this.#start = true;
+    this.moveTo(0, 0);
   }
 
   moveTo(x, y) {
     this.#x = x;
     this.#y = y;
+    this.#ltr = true;
+    this.#ttb = true;
+    this.#start = true;
     this.#path2d.moveTo(x, y);
   }
 
-  lineTo(x, y) {
-    this.#x = x;
+  // Never use this.
+  // lineTo(x, y) {
+  //   this.#x = x;
+  //   this.#y = y;
+  //   this.#path2d.lineTo(x, y);
+  // }
+/*
+
+-----
+
+---
+    |
+___
+
+1.5, 0  ___  0, 1/2
+       /   \
+       |   |
+1, 1.5 \___/ 1/2, 1
+*/
+  // if ltr && ttb
+  // start && end
+  // ------------
+  // start && !end
+  // --------
+  // !start && !end
+  //   \_______
+  // !start && end
+  // \____________
+
+  verticalTo(y, end = false) {
+    if (this.#y === y) return;
+    this.#ttb = this.#y < y;
+    const AR = this.#arcRadius;
+    const XOR = ((this.#ltr ? !this.#ttb : this.#ttb) ? AR : -AR);
+    // console.log(caller(), 'V', this.#start, 'end', end, 'y', y, 'this.#y', this.#y, 'ltr', this.#ltr, 'ttb', this.#ttb,'XOR', XOR, 'cx', this.#cx, 'cy', this.#cy)
+    if (this.#start) {
+      // No need to do an arc from prev, stop before actual x
+    } else {
+      // connect previous cx
+      this.path2d.moveTo(this.#cx, this.#y);
+      this.#path2d.arcTo(this.#x, this.#y, this.#x, y, AR);
+      this.#cy = this.#y + (this.#ttb ? AR : -AR);
+      // this.#path2d.lineTo(this.#x, this.#cy);
+    }
+    if (end) {
+      // Connect to end without an arc
+      this.#path2d.lineTo(this.#x, y);
+      this.#cx = this.#x;
+      this.#cy = y;
+    } else {
+      // keep space for arc
+      this.#cy = y - (this.#ttb ? AR : -AR);
+      this.#path2d.lineTo(this.#x, this.#cy);
+    }
+    this.#start = end;
     this.#y = y;
-    this.#path2d.lineTo(x, y);
   }
 
-  verticalTo(y) {
-    this.#y = y;
-    this.#path2d.lineTo(this.#x, y);
-  }
-
-  horizontalTo(x) {
+  horizontalTo(x, end = false) {
+    if (this.#x === x) return;
+    this.#ltr = this.#x < x;
+    const AR = this.#arcRadius;
+    const XOR = ((this.#ltr ? !this.#ttb : this.#ttb) ? AR : -AR);
+    // console.log(caller(), 'H', 'start', this.#start, 'end', end, 'x', x, 'this.#x', this.#x, 'ltr', this.#ltr, 'ttb', this.#ttb,'XOR', XOR, 'cx', this.#cx, 'cy', this.#cy)
+    if (this.#start) {
+      // No need to do an arc from prev, stop before actual x
+    } else {
+      // connect previous cx
+      this.path2d.moveTo(this.#x, this.#cy);
+      this.#path2d.arcTo(this.#x, this.#y, x, this.#y, AR);
+      this.#cx = this.#x + (this.#ltr ? AR : -AR);
+      // this.#path2d.lineTo(this.#cx, this.#y);
+    }
+    if (end) {
+      // Connect to end without an arc
+      this.#path2d.lineTo(x, this.#y);
+      this.#cx = x;
+      this.#cy = this.#y;
+    } else {
+      // keep space for arc
+      this.#cx = x - (this.#ltr ? AR : -AR);
+      this.#path2d.lineTo(this.#cx, this.#y);
+    }
+    this.#start = end;
     this.#x = x;
-    this.#path2d.lineTo(x, this.#y);
   }
 }
-
 class RailroadElement extends HTMLElement {
   connectedCallback() {
     // this.parentElement.draw();
@@ -72,9 +152,11 @@ class RailroadElement extends HTMLElement {
     if (!this.hasAttribute('optional')) return;
 
     const { x, y } = path;
-    path.verticalTo(this.offsetTop - gap.block);
-    path.horizontalTo(this.offsetLeft + this.offsetWidth + gap.midInline);
+    path.horizontalTo(x + gap.quarterInline);
+    path.verticalTo(this.offsetTop - gap.midBlock);
+    path.horizontalTo(x + this.offsetWidth + gap.midInline + gap.quarterInline);
     path.verticalTo(y);
+    path.horizontalTo(this.offsetLeft + this.offsetWidth + gap.midInline, true);
     path.x = x;
   }
 
@@ -82,22 +164,20 @@ class RailroadElement extends HTMLElement {
     if (!this.hasAttribute('repeatable')) return;
 
     const { x, y } = path;
-    path.verticalTo(this.offsetTop + this.offsetHeight + gap.block);
-    path.horizontalTo(this.offsetLeft + this.offsetWidth + gap.midInline);
+    path.moveTo(x + gap.midInline + gap.quarterInline, y);
+    path.horizontalTo(x + gap.quarterInline);
+    path.verticalTo(this.offsetTop + this.offsetHeight + gap.midBlock);
+    path.horizontalTo(this.offsetLeft + this.offsetWidth + gap.midInline - gap.quarterInline);
     path.verticalTo(y);
+    path.horizontalTo(this.offsetLeft + this.offsetWidth, true);
     path.x = x;
   }
 
-  drawAttributes(path, gap) {
+  draw(path, gap, end) {
+    const { x, width } = this.getRect();
     this.#drawOptional(path, gap);
     this.#drawRepeatable(path, gap);
-  }
-
-  draw(path, gap) {
-    const { x, midHeight, width } = this.getRect();
-    path.lineTo(x + 1, midHeight);
-    path.x += width - 2;
-    path.horizontalTo(path.x + gap.midInline + 1);
+    path.horizontalTo(x + width + gap.midInline, end);
   }
 
   getRect() {
@@ -147,7 +227,8 @@ class RailroadContainer extends RailroadElement {
       block: Math.round(block),
       inline: Math.round(inline),
       midBlock: Math.round(block / 2),
-      midInline: Math.round(inline / 2)
+      midInline: Math.round(inline / 2),
+      quarterInline: Math.round(inline / 4)
     };
   }
 }
@@ -159,20 +240,20 @@ class RailroadSequence extends RailroadContainer {
       const child = this.children[i],
         { midHeight } = child.getRect();
 
+      // TODO: the diagram's leftmost is cut by 0.5 * lineWidth
       if (path.x > child.offsetLeft) {
         let prev = this.children[i-1];
-        path.horizontalTo(prev.offsetLeft + prev.offsetWidth + gap.inline);
+        path.horizontalTo(prev.offsetLeft + prev.offsetWidth + gap.midInline);
         path.verticalTo(prev.offsetTop + lineHeight + gap.midBlock);
         path.horizontalTo(child.offsetLeft - gap.inline);
         path.verticalTo(midHeight);
-        path.horizontalTo(child.offsetLeft - gap.midInline);
+        path.horizontalTo(child.offsetLeft - gap.midInline, true);
         lineHeight = child.offsetHeight;
       } else if (child.offsetHeight > lineHeight) {
         lineHeight = child.offsetHeight;
       }
 
-      child.drawAttributes(path, gap);
-      child.draw(path, gap);
+      child.draw(path, gap, true);
     }
   }
 }
@@ -180,16 +261,24 @@ class RailroadSequence extends RailroadContainer {
 class RailroadChoice extends RailroadContainer {
   draw(path, gap) {
     const { x, y } = path;
-    for (const child of this.children) {
+    // TODO: first child is problematic :/
+    // TODO: see migrate
+    // TODO: if optional, sequence line crosses
+    super.draw(path, gap)
+    const me = this.getGap();
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i];
+      const first = i === 0;
+      path.horizontalTo(this.offsetLeft + me.quarterInline);
       path.verticalTo(child.getRect().midHeight);
-      child.drawAttributes(path, gap);
-      child.draw(path, gap);
-      path.horizontalTo(this.offsetLeft + this.offsetWidth + gap.midInline);
+      path.horizontalTo(this.offsetLeft + me.quarterInline);
+      child.draw(path, me, true);
+      path.horizontalTo(this.offsetLeft + this.offsetWidth - me.midInline + gap.quarterInline, first);
       path.verticalTo(y);
+      path.horizontalTo(this.offsetLeft + this.offsetWidth + gap.midInline, true);
       path.x = x;
     }
-
-    path.x += this.offsetWidth + gap.inline;
+    path.x += this.offsetWidth + gap.midInline;
   }
 }
 
@@ -228,11 +317,11 @@ class RailroadDiagram extends RailroadSequence {
     const computedStyle = getComputedStyle(this);
     const arcRadius = fromPx(computedStyle.getPropertyValue('--arc-radius')) || 8;
     const lineWidth = fromPx(computedStyle.getPropertyValue('--line-width')) || 4;
-
     const path = new Path(this.#canvas, arcRadius);
+    path.moveTo(this.getGap().midInline, this.children[1].getRect().midHeight); // TODO: Start
     super.draw(path, this.getGap(), 1);
-    
-    // TODO: use css custome property - should be the same as borders
+    path.horizontalTo(this.offsetLeft + this.offsetWidth - this.getGap().midInline, true);// TODO: End
+
     this.#ctx.lineWidth = lineWidth;
     this.#ctx.scale(devicePixelRatio, devicePixelRatio);
     this.#ctx.stroke(path.path2d);
